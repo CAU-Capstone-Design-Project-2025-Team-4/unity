4a6beab9b18ae7a394dbd3e95ee95f2700c69aa4
boy-min <sonhyunmin00@naver.com>
2025-05-19 14:54:01 +0900

    feat: add ModelPropertiesDto

---
+++ Assets/Scripts/Dto/ModelPropertiesDto.cs
@@ -0,0 +1,10 @@
+namespace Prism.Dto
+{
+    [System.Serializable]
+    public class ModelPropertiesDto
+    {
+        private string id;
+        private TransformDto transform;
+        private string shader;
+    }
+}
